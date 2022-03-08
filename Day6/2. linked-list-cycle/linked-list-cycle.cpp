74b56df600bd0ff7d61dd8a852d5168eb27fd444
Sahib pratap singh <sahibpratapsingh@gmail.com>
2022-03-08 05:55:00 +0530

    Updated on 8-03-2022

--- Day6/2. linked-list-cycle/linked-list-cycle.cpp
+++ Day6/2. linked-list-cycle/linked-list-cycle.cpp
@@ -1,8 +1,36 @@
+//Method 1
+
 //Using hashing
+//Here we will chech if the node we have visisted is present or not if we have visited it then it mean there is cycle.
+
 //Time complexity: O(n)
 //Space complexity: O(n)
 
+class Solution {
+public:
+    unordered_map<ListNode*,int> mp;
+    bool hasCycle(ListNode *head) {
+        while(head!=NULL){
+            if(mp.find(head) == mp.end()){
+                mp[head]++;
+            }
+            else{
+                return true;
+            }
+            head=head->next;
+        }
+        return false;
+    }
+};
+
+//Method 2
+
 //Floyd’s Cycle-Finding Algorithm 
+//Here we will use two pointers slow and fast 
+//where slow will travel one step and fast two step
+//if they become same at some time there is cycle
+//if fast is null then there is no cycle
+
 //Time complexity: O(n)
 //Space complexity: O(1)
 
@@ -25,8 +53,11 @@ public:
     }
 };
 
+//Method 3
+
 //One easy methos is to modify the values of the linked list to tha value which is not in the range.
 //When you encounter this value again then there is loop
+
 //Time complexity: O(n)
 //Space complexity: O(1)
 
